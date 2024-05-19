af3b7a555828f8720e86591e4bae4f8e9f3e6523
Andrew Muhsen Paramedis <Andrew.naaem99@eng-st.cu.edu.eg>
2024-05-19 17:04:53 +0300

    fix

--- perplexity_subprocess.py
+++ perplexity_subprocess.py
@@ -3,36 +3,37 @@ import time
 import subprocess
 import concurrent.futures
 
-start_time = time.time()
-
-argparser = argparse.ArgumentParser()
-argparser.add_argument("--model_name", type=str, help="model name")
-argparser.add_argument("--load_in", type=str, default="4bit")
-argparser.add_argument("--total_size", type=int)
-argparser.add_argument("--splits", type=int)
-argparser.add_argument("--split_size", type=int)
-argparser.add_argument("--output_dir", type=str)
-args = argparser.parse_args()
-
 def run_command(command):
     process = subprocess.Popen(command, shell=True)
     process.communicate()
 
-TOTAL_SIZE = args.total_size #256000
-SPLITS = args.splits #4
-SPLIT_SIZE = args.split_size #1000
-PART = TOTAL_SIZE // SPLITS
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--model_name", type=str, help="model name")
+    argparser.add_argument("--load_in", type=str, default="4bit")
+    argparser.add_argument("--total_size", type=int)
+    argparser.add_argument("--splits", type=int)
+    argparser.add_argument("--split_size", type=int)
+    argparser.add_argument("--output_dir", type=str)
+    args = argparser.parse_args()
 
-command_list = []
-for split in range(SPLITS):
-    model_name = f"{args.model_name}_{split}"
-    command = f'python calc_perplexity.py --model_name "{model_name}" --min_range {split*PART} --max_range {(split+1)*PART} --split_size {args.split_size} --load_in "{args.load_in}" "{args.output_dir}"'
-    command_list.append(command)
+    start_time = time.time()
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(run_command, command_list)
-    
-end_time = time.time()
-total_time = end_time - start_time
+    TOTAL_SIZE = args.total_size #256000
+    SPLITS = args.splits #4
+    SPLIT_SIZE = args.split_size #1000
+    PART = TOTAL_SIZE // SPLITS
 
-print(f"Total execution time: {total_time} seconds")
+    command_list = []
+    for split in range(SPLITS):
+        model_name = f"{args.model_name}_{split}"
+        command = f'python calc_perplexity.py --model_name "{model_name}" --min_range {split*PART} --max_range {(split+1)*PART} --split_size {args.split_size} --load_in "{args.load_in}" "{args.output_dir}"'
+        command_list.append(command)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(run_command, command_list)
+        
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    print(f"Total execution time: {total_time} seconds")
